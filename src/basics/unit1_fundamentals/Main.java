0deaab3e6590229276b547cdc3b6e07770a711b9
NOEMI GP <noemigp0@gmail.com>
2023-05-23 21:34:28 +0100

    Adding first excersises

---
+++ src/basics/unit1_fundamentals/Main.java
@@ -0,0 +1,10 @@
+package basics.unit1_fundamentals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Cat");
+        System.out.println("Dog");
+        System.out.println("Rabbit");
+    }
+}
---
+++ src/basics/unit1_handson/CodingChallenge1.java
@@ -0,0 +1,42 @@
+package basics.unit1_handson;
+
+public class CodingChallenge1 {
+    public static void main(String args[]){
+        CodingChallenge1 codingChallenge1Obj = new CodingChallenge1();
+        //codingChallenge1Obj.firstSolution();
+        //codingChallenge1Obj.secondSolution();
+        codingChallenge1Obj.exercise3();
+
+
+
+    }
+
+    public void firstSolution(){
+        System.out.println("SHOPPING LIST");
+        System.out.println("\t- Milk");
+        System.out.println("\t\t> Semmi-skimmed");
+        System.out.println("\t\t> Medium");
+        System.out.println("\t- Eggs");
+        System.out.println("\t\t> Fre-range");
+        System.out.println("\t- Bread");
+
+    }
+
+    public void secondSolution(){
+        System.out.print("SHOPPING LIST \n" +
+                "\t- Milk\n" +
+                "\t\t> Semmi-skimmed\n" +
+                "\t\t> Meidum\n" +
+                "\t- Eggs\n" +
+                "\t\t> Free-range\n" +
+                "\t- Bread");
+    }
+
+    public void exercise3(){
+        System.out.print("The\n");
+        System.out.print("\tCake\n");
+        System.out.print("Is\t\t\t");
+        System.out.print("A\n");
+        System.out.print("\tLie.");
+    }
+}
