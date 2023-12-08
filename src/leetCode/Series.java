c3f849f597f1d006227aefcac442df19eb764995
NOEMI GP <noemigp0@gmail.com>
2023-12-08 13:24:19 -0600

    Adding solution to HR Series challenge

--- src/leetCode/Series.java
+++ src/leetCode/Series.java
@@ -3,34 +3,42 @@ package leetCode;
 import java.util.Scanner;
 
 public class Series {
-    /*
+    /*https://www.hackerrank.com/challenges/java-loops/problem
     * q = queries --> indicates the number of queries the program must ask the input
-    *  a= 5 b = 3 n = 5
-    *  5 + 2^0 * 3
-    *  8
+    *  a= 0 b = 2 n = 10
+    *  0 + 2^0 * 2
+    *  2
+    * 2 + (2^1 * b) = 6
+    * 6
+    * 6 + (2^2 * b) = 6
     *  2 6 14
     * */
 
     public static void main(String args[]){
 
         Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
 
-        int a=0,b=0,n =-1;
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++ ){
-             a = in.nextInt();
-             b = in.nextInt();
-             n = in.nextInt();
+        for(int i=0;i<t;i++){
+            String serie = "";
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
 
+            for (int j = 0; j < n; j++) {
+                a = (int) (a + ((Math.pow(2,j) * b)));
+                serie += a + " ";
+
+            }
+            System.out.println(serie.substring(0,serie.length()-1));
         }
-        int counter = 0;
-        double acumulator = 0;
+        in.close();
 
-        while (n > counter){
-            acumulator  = acumulator + (a + (Math.pow(2, counter))) * b;
-            System.out.println(acumulator);
-            counter++;
-        }
+
+
+
+
+
 
 
     }
