79875741a7972e81da48eccd208f4e81c93aa914
NOEMI GP <noemigp0@gmail.com>
2023-12-15 12:54:35 -0600

    Adding Prime numbers solution

---
+++ src/basics/handson/PrimeNumbers.java
@@ -0,0 +1,25 @@
+package basics.handson;
+
+public class PrimeNumbers {
+    //Write a program which print all the prime numbers from 1 to 1000
+    public static void main(String args[]) {
+
+        int initialNumber = 2;
+
+        while (initialNumber <= 1000) {
+            int counter = 0;
+            for (int div = 1; div <= initialNumber; div++) {
+                if ((initialNumber % div == 0) && (initialNumber % 1 == 0)) {
+                    counter = counter +1;
+                }
+            }
+            if (counter == 2){
+                System.out.println(initialNumber + " is Prime");
+            }
+        initialNumber++;
+        }
+
+
+
+    }
+}
