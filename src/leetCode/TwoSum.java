3ac8828fe54be4decb36509a06dd396bae8a3129
NOEMI GP <noemigp0@gmail.com>
2023-12-18 14:25:31 -0600

    Second version to the Two Sum problem, 60 of 61 test cases were passed with this solution

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -1,11 +1,36 @@
 package leetCode;
 
 public class TwoSum {
+
+    /*
+    * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+           * Example 1:
+            Input: nums = [2,7,11,15], target = 9
+            Output: [0,1]
+            Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+            * Example 2:
+            Input: nums = [3,2,4], target = 6
+            Output: [1,2]
+
+            * Example 3:
+            Input: nums = [3,3], target = 6
+            Output: [0,1]
+        *
+    * */
     public static void main (String args[]){
         //int[] nums = new int[]{2,7,11,15};
+        //int target = 9;
         //int[] nums = new int[]{3,3};
-        int[] nums = new int[]{3,2,4};
+        //int target = 6;
+        //int[] nums = new int[]{3,2,4};
+        //int target = 6;
+        int[] nums = new int[]{3,2,3};
         int target = 6;
+
         int[] results = twoSum(nums, target);
 
 
@@ -21,30 +46,23 @@ public class TwoSum {
     }
 
     public static int[] twoSum(int[] nums, int target){
-        int [] result = new int[2];
-        int previousIndex = 0;
+        int [] results = new int[2];
+
 
         for ( int i = 0; i < nums.length; i++){
-
-
-            if( i > 0 ){
-
-                if((nums[i] + nums[previousIndex]) == target ){
-                    result[0] = previousIndex;
-                    result[1] = i;
-                    break;
-
+            for(int j = nums.length -1; j >= 0; j--){
+                if(nums[i] + nums[j] == target){
+                    results[0] = i;
+                    results[1] = j;
                 }
 
             }
-
-            previousIndex = i;
 
         }
 
 
 
 
-        return result;
+        return results;
     }
 }
